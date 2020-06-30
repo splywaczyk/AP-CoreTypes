0c27d8637f57751bd0c6f36a0898413a79396553
Seweryn Plywaczyk <s.plywaczyk@gmail.com>
2020-06-30 09:28:46 +0200

    Implementation of Allocator (#50)
    * Initial implementation
    
    * Add new line at the EOF
    
    * Define member types and consturctors for ara::core::Vector. Add aliases to std::allocator and std::allocator_traits
    
    * Adapt interface to C++11. Full implementation - not fully tested yet
    
    * Remove not necessary code
    
    * Delete allocator.h - will be moved to other PullRequest. Apply some review remarks for vector.h
    
    * Allocator as alias for std::allocator
    
    * remove vector.h from allocator branch
    
    * Remove redundant underscore
    
    * Fix typo

---
+++ include/ara/core/allocator.h
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2020
+ * umlaut Software Development and contributors
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef ARA_CORE_ALLOCATOR_H_
+#define ARA_CORE_ALLOCATOR_H_
+
+#include <memory> // std::allocator
+
+namespace ara::core
+{
+
+/**
+* @brief Allocator
+*
+* @todo Define custom Allocator 
+*/
+template<class T>
+using Allocator = std::allocator<T>;
+
+/**
+* @brief AllocatorTraits
+*
+* @todo Define custom AllocatorTraits 
+*/
+template<class Alloc>
+using AllocatorTraits = std::allocator_traits<Alloc>;
+
+} // namespace ara::core
+
+#endif // ARA_CORE_ALLOCATOR_H_
